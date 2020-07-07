a767745c594e7095142ebc7bb9b4deb5cf80b4ec
viniaf13 <viniaf13@gmail.com>
2020-07-07 18:45:56 -0300

    implemented player movement

--- 2D Trollercoaster/Assets/Prefabs/Player.cs
+++ 2D Trollercoaster/Assets/Prefabs/Player.cs
@@ -1,18 +1,84 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Player Parameters")]
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    [Header("Body Elements")]
+    [SerializeField] GameObject body = default;
+    [SerializeField] GameObject feet = default;
+
+    private Rigidbody2D myRigidBody;
+    private Animator myAnimator;
+    private Collider2D bodyCollider;
+    private Collider2D feetCollider;
+
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        bodyCollider = body.GetComponent<Collider2D>();
+        feetCollider = feet.GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        HandleAnimation();
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        float inputX = Input.GetAxis("Horizontal") * moveSpeed;
+        myRigidBody.velocity = new Vector2(inputX, myRigidBody.velocity.y);
+
+        bool playerHasXMovement = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasXMovement)
+        {
+            FlipSprite();
+        }
+    }
+
+    private void HandleAnimation()
+    {
         
+        bool playerHasYMovement = !IsOnGround();
+        myAnimator.SetBool("isLanded", !playerHasYMovement);
+        if (playerHasYMovement)
+        {
+            bool isHeJumping = (Mathf.Sign(myRigidBody.velocity.y) > 0);
+            myAnimator.SetBool("isJumping", isHeJumping);
+        }
+        else
+        {
+            bool playerHasXMovement = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+            //bool playerHasXMovement = Mathf.Abs(myRigidBody.velocity.x) > 7f;
+            myAnimator.SetBool("isRunning", playerHasXMovement);
+        }
+    }
+
+    private void Jump()
+    {
+        if (!IsOnGround()) return;
+        if (Input.GetButtonDown("Jump"))
+        {
+            myRigidBody.velocity = new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    private void FlipSprite()
+    {
+        transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x),1f);
+    }
+
+    private bool IsOnGround()
+    {
+        return feetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
     }
 }
