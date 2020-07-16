edfc6bd7bd94e8fea8409f33014d0b33cfc30a03
viniaf13 <viniaf13@gmail.com>
2020-07-16 15:30:37 -0300

    Added a menu screen

--- 2D Trollercoaster/Assets/Scripts/LevelLoader.cs
+++ 2D Trollercoaster/Assets/Scripts/LevelLoader.cs
@@ -8,19 +8,32 @@ public class LevelLoader : MonoBehaviour
     [SerializeField] float loadDelay = 3f;
 
     private int currentSceneIndex;
+    private enum Levels
+    {
+        MainMenu = 0,
+        LevelSelectionScreen = 1,
+        Level1 = 2,
+    }
 
     private void Start()
     {
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
-    private IEnumerator LoadLevel(int sceneIndex)
+    private IEnumerator LoadLevelWithDelay(int sceneIndex)
     {
         yield return new WaitForSeconds(loadDelay);
         SceneManager.LoadScene(sceneIndex);
         currentSceneIndex = sceneIndex;
     }
+
+    private void LoadLevelInstant(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+        currentSceneIndex = sceneIndex;
+    }
+
     public void RestartLevel()
     {
-        StartCoroutine(LoadLevel(currentSceneIndex));
+        StartCoroutine(LoadLevelWithDelay(currentSceneIndex));
     }
 }
