f6bc8198d2a40cbbd26cce920214b9f8ab32eb7a
viniaf13 <viniaf13@gmail.com>
2020-07-17 18:15:29 -0300

    made level 1 design

---
+++ 2D Trollercoaster/Assets/Scripts/Trolls/BodyToDynamic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyToDynamic : MonoBehaviour
+{
+    [SerializeField] int newMass = 1;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.GetComponentInParent<Player>())
+        {
+            Rigidbody2D myRigidBody = GetComponent<Rigidbody2D>();
+            myRigidBody.bodyType = RigidbodyType2D.Dynamic;
+            myRigidBody.mass = newMass;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponentInParent<Player>())
+        {
+            Rigidbody2D myRigidBody = GetComponent<Rigidbody2D>();
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            myRigidBody.mass = newMass;
+        }
+    }
+}
