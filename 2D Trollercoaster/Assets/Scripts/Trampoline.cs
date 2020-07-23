6f79feec5459f1234b6a03cb9ba39c8d65fa95b5
viniaf13 <viniaf13@gmail.com>
2020-07-23 14:55:26 -0300

    made a trampoline object

---
+++ 2D Trollercoaster/Assets/Scripts/Trampoline.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    [SerializeField] private float bumpVelocity = 30f;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player)
+        {
+            bool hitAtTop = CheckForCollisionAtTop(other);
+            if (hitAtTop)
+            {
+                Rigidbody2D playerRb = other.gameObject.GetComponent<Rigidbody2D>();
+                if (playerRb)
+                {
+                    animator.SetTrigger(Constants.Animations.Touched);
+                    playerRb.velocity = new Vector2(0f, bumpVelocity);
+                }
+            }
+        }
+    }
+    private bool CheckForCollisionAtTop(Collision2D other)
+    {
+        bool hitAtTop = false;
+        foreach (ContactPoint2D point in other.contacts)
+        {
+            hitAtTop = (Mathf.Abs(point.normal.y) >= 0.9f);
+        }
+        return hitAtTop;
+    }
+}
